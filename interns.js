17bc2911d8d234aa215716b87a52c896f0fdb03f
Kenny <kennygbeminiyi@gmail.com>
2020-04-15 22:46:05 +0100

    Add files via upload

---
+++ interns.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+MongoClient.connect(url, function(err, db) {
+if (err) throw err;
+dba = db.db('mydb')
+var collection = dba.collection("myMovies")
+collection.insertMany([
+	{movie: "The Banker", year:"2020", rating: 8},
+	{movie: "Bad Boys", year:"2020", rating: 7},
+	{movie: "The Hunt", year:"2020", rating: 7},
+	{movie: "Bloodshot", year:"2020", rating: 7.5},
+	{movie: "First Cow", year:"2020", rating: 6.5}
+	], function(err, result){
+		if (err) throw err;
+		console.log(result)
+	}
+	)
+
+});
